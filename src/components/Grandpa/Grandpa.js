7ba89d9880f84c59424795a20bb60f8be068ee36
Md Hasanul Karim <hasanlink71@yahoo.com>
2022-10-15 23:59:12 +0600

    context api working

--- src/components/Grandpa/Grandpa.js
+++ src/components/Grandpa/Grandpa.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import Aunt from '../Aunt/Aunt';
 import Father from '../Father/Father';
 import Uncle from '../Uncle/Uncle';
 import './Grandpa.css'
+
+export const RingContext = createContext('matir ring');
 const Grandpa = () => {
+  const ring = 'diamond ring'
   const house = 7;
-  return (
+  return (<RingContext.Provider value='valuable ring'>
     <div className='grandpa'>
       <h1>This is our grandpa</h1>
       <section className='flex'>
@@ -13,7 +16,7 @@ const Grandpa = () => {
         <Uncle house={house}></Uncle>
         <Aunt house={house}></Aunt>
       </section>
-    </div>
+    </div></RingContext.Provider>
   );
 };
 
--- src/components/Myself/Myself.js
+++ src/components/Myself/Myself.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { RingContext } from '../Grandpa/Grandpa';
 
 const Myself = ({ house }) => {
+  const ring = useContext(RingContext)
   return (
     <div>
       <h1>Myself</h1>
       <p>House: {house}</p>
+      <p>House: {ring}</p>
     </div>
   );
 };
